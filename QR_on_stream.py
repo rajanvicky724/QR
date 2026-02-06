4333f1bfcec521b88c53605ce6eb534f776cf228
rajanvicky724 <rajanvicky724@gmail.com>
2026-02-06 15:20:20 +0530

    Update QR_on_stream.py

--- QR_on_stream.py
+++ QR_on_stream.py
@@ -138,12 +138,13 @@ if uploaded_pdf and uploaded_csv:
                     data=output_buffer,
                     file_name="output_qr.pdf",
                     mime="application/pdf",
-                    type="primary"
-            button(username="https://buymeacoffee.com/vigneshna", floating=True, width=221),   
+                    type="primary",
+            button(username="https://buymeacoffee.com/vigneshna", floating=True, width=221) 
                 )     
             except Exception as e:
                 st.error(f"Error: {e}")
                 
+
 
 
 
